4d2dd05f302249d4517efd781ce1ab316efb68a0
navestelar <acch.escola@gmail.com>
2024-01-24 20:07:54 -0300

    renaming main to home page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Main from './pages/Main'
+import Main from './pages/home'
 import Error from './pages/Error'
 
 export function AppRoutes() {
