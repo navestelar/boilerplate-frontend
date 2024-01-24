776bd14522ee2f4175cb0bcf34ded2f6ffc78114
navestelar <acch.escola@gmail.com>
2024-01-24 20:05:44 -0300

    feat: 🎸 Not fount initial page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
+import Error from './pages/Error'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='*' element={<Error />} />
     </Routes>
   )
 }
